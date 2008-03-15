cb27288de47a6ec78daef51982e1b5b70045afb0
Frédéric de Villamil <frederic@de-villamil.com>
2008-03-15 23:33:32 +0000

    import

---
+++ init.rb
@@ -0,0 +1,5 @@
+require 'sidebar'
+require 'recentposts_sidebar'
+
+RecentPostsSidebar.view_root = File.dirname(__FILE__) + '/views'
+
---
+++ lib/recentposts_sidebar.rb
@@ -0,0 +1,12 @@
+class RecentPostsSidebar < Sidebar
+  description "Displays the last posts published"
+
+  setting :title,     "Recent Posts", :label => "Title"
+  setting :count,     10, :label => "Number of Posts"
+  setting :post_length,     150, :label => "Size of the post to be displayed"
+
+  def recent_posts
+     @recent_posts = Article.find(:all, :conditions => "published = 1", :order => 'published_at DESC', :limit => count)
+  end
+  
+end
